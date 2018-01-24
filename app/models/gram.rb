a7b168355e24dbac2177ef7702b3770cda94adf6
Madelyn Hobbs <madelyn.hobbs@gmail.com>
2018-01-23 21:28:27 -0600

    working

--- app/models/gram.rb
+++ app/models/gram.rb
@@ -1,5 +1,5 @@
 class Gram < ApplicationRecord
-  validates :title, presence: true
+  validates :title, presence: true, length: { maximum: 50, minimum: 3 }
   validates :message, presence: true
   has_many :comments
 
